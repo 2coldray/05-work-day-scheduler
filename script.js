deebf7b2341e2bd3d6e16181145bb1ed862bd0e6
Ray Bray <rbray.bray2@gmail.com>
2020-09-11 23:27:29 -0400

    Added another row to the jumbotrom because I want to display the current time. This will be used later

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
+$(document).ready(function(){
 //Display Date on page
 //Create element to store current date and time
 var now = moment().format("dddd, MMMM Do YYYY");
-console.log(now);
+// console.log(now);
 //Add to Current Day ID
 $("#currentDay").text(now);
 //===============================================
+
+//Make variable for current hour
+var currentHour = moment().format("HH");
+console.log(currentHour);
+
+//Make an array of all the hours
+var workHours = ["09", "10", "11", "12", "13", "14", "15", "16", "17"];
+
+//Append 
+
+
+
+
+});
+
