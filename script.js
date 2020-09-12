12db9a954a8a86f39f0dd28fb9c7eee5bb239e40
Ray Bray <rbray.bray2@gmail.com>
2020-09-12 18:30:55 -0400

    App is finished. Now time to work on the README

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ var currentTime = moment().format("h:mm A");
 var sampleHour = moment().format("H")
 
 //Display currentTime to Page
-$("#currentTime").text(currentTime);
+// $("#currentTime").text(currentTime);
 console.log(currentTime);
 console.log(currentHour);
 console.log(sampleHour);
@@ -24,7 +24,22 @@ var nine = $("#text9");
 nine = 9;
 var ten = $("#text10");
 ten = 10;
+var eleven = $("#text11");
+eleven = 11;
+var twelve = $("#text12");
+twelve = 12;
+var thirteen = $("#text13");
+thirteen = 13;
+var fourteen = $("#text14");
+fourteen = 14;
+var fifteen = $("#text15");
+fifteen = 15;
+var sixteen = $("#text16");
+sixteen = 16;
+var seventeen = $("#text17");
+seventeen = 17;
 
+//Assigning the blocks which colors to show based upon the time
 if (currentHour > nine) {
     $("#text9").addClass("past")
 } else if (currentHour == nine) {
@@ -39,23 +54,63 @@ if (currentHour > ten) {
     $("#text10").addClass("present");
 } else {
     $("#text10").addClass("future");
-}
+};
 
+if (currentHour > eleven) {
+    $("#text11").addClass("past");
+} else if (currentHour == eleven) {
+    $("#text11").addClass("present");
+} else {
+    $("#text11").addClass("future");
+};
 
+if (currentHour > twelve) {
+    $("#text12").addClass("past");
+} else if (currentHour == twelve) {
+    $("#text12").addClass("present");
+} else {
+    $("#text12").addClass("future");
+};
 
+if (currentHour > thirteen) {
+    $("#text13").addClass("past");
+} else if (currentHour == thirteen) {
+    $("#text13").addClass("present");
+} else {
+    $("#text13").addClass("future");
+};
 
-// Make an array of all the hours 
-// var workHours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+if (currentHour > fourteen) {
+    $("#text14").addClass("past");
+} else if (currentHour == fourteen) {
+    $("#text14").addClass("present");
+} else {
+    $("#text14").addClass("future");
+};
 
-// var textOne = $("#text9").attr("hour", workHours[0]);
+if (currentHour > fifteen) {
+    $("#text15").addClass("past");
+} else if (currentHour == fifteen) {
+    $("#text15").addClass("present");
+} else {
+    $("#text15").addClass("future");
+};
 
-// if (textOne < sampleHour) {
-//     console.log(textOne.addClass("past"));
-// } else if (textOne == sampleHour) {
-//     console.log(textOne.addClass("present"));
-// } else {
-//     console.log(textOne.addClass("future"));
-// };
+if (currentHour > sixteen) {
+    $("#text16").addClass("past");
+} else if (currentHour == sixteen) {
+    $("#text16").addClass("present");
+} else {
+    $("#text16").addClass("future");
+};
+
+if (currentHour > seventeen) {
+    $("#text17").addClass("past");
+} else if (currentHour == seventeen) {
+    $("#text17").addClass("present");
+} else {
+    $("#text17").addClass("future");
+};
 
 //Setting local storage
 $("#save9").on("click", function() {
