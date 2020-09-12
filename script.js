3e0f94e1b5ad94ee47114e75a72ef3fd32f020dc
Ray Bray <rbray.bray2@gmail.com>
2020-09-12 01:07:33 -0400

    Added the future class to the textarea

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+
 //Display Date on page
 //Create element to store current date and time
 var now = moment().format("dddd, MMMM Do YYYY");
@@ -16,10 +17,11 @@ $("#currentTime").text(currentTime);
 console.log(currentTime);
 console.log(currentHour);
 
-//Make an array of all the hours
+//Make an array of all the hours 
 var workHours = ["09", "10", "11", "12", "13", "14", "15", "16", "17"];
 
-//Append 
+//Apply future class to the textarea
+$("textarea").addClass("future");
 
 
 
